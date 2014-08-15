fce04b1db5af12c01f2f7f9b88e9bd1d00c9b49d
David Heryanto <david.heryanto@hotmail.com>
2014-08-16 00:55:18 +0800

    Add graphs

---
+++ software/namd/README.md
@@ -0,0 +1,2 @@
+![CUDA vs non-CUDA](apoa1_result/cuda_vs_non-cuda.png "CUDA vs non-CUDA")
+![TCP vs non-TCP](apoa1_result/tcp_vs_non-tcp.png "TCP vs non-TCP")
---
+++ software/namd/result.py
@@ -0,0 +1,48 @@
+import glob
+import argparse
+import re
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def process_result(folder_path):
+    f = {}
+
+    for file_path in glob.glob(folder_path + '/result/*'):
+        n_core = int(re.findall(r'n_core-[\d]+', file_path)[0][7:])
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            results = re.findall(r'[\d\.?\d*]+', lines[-2])
+
+            if len(results) < 1:
+                continue
+
+            cpu_time = float(results[1])
+            f[n_core] = cpu_time
+
+    plt.plot(list(f.keys()), list(f.values()))
+    plt.title(folder_path)
+    plt.xlabel('N Cores')
+    plt.ylabel('CPU Time (s)')
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-i', '--input')
+    # args = parser.parse_args()
+
+    result_dirs = [
+        'NAMD_2.9_Linux-x86_64-TCP',
+        'NAMD_2.9_Linux-x86_64-multicore',
+        'NAMD_2.9_Linux-x86_64-multicore-CUDA'
+    ]
+
+    for dir in result_dirs:
+        process_result(dir + "/apoa1")
+
+    plt.title('CPU Time vs No of Cores for apoa1 Input')
+    plt.legend(result_dirs, loc='center')
+    plt.show()
+
+
