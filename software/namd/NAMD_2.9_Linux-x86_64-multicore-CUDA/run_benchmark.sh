112e30b671fc2cfa8a8883078e40818c676c9bfc
David Heryanto <david.heryanto@hotmail.com>
2014-08-16 12:55:23 +0800

    Add TCP stmv

--- software/namd/NAMD_2.9_Linux-x86_64-multicore-CUDA/run_benchmark.sh
+++ software/namd/NAMD_2.9_Linux-x86_64-multicore-CUDA/run_benchmark.sh
@@ -1,4 +1,9 @@
-for i in {1..24}
+for i in {0..24..4}
 do
-	./namd2 +idlepoll +p$i stmv/stmv.namd > stmv/result/n_core-`printf %02d $i`.txt
+	if [$i -eq 0]
+	then
+		./namd2 +idlepoll +p1 stmv/stmv.namd > stmv/result/n_core-1.txt
+	else	
+		./namd2 +idlepoll +p$i stmv/stmv.namd > stmv/result/n_core-`printf %02d $i`.txt
+	fi
 done
